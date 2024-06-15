032e337c2ebefb9d28d0467fe2171d570e5c3d20
Leonard Zonaphan <leonard.zonaphan@gdn-commerce.com>
2024-06-15 12:32:38 +0700

    update navbar to floating when scrolled down

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,18 +1,42 @@
+<script setup>
+import { ref, onMounted, onUnmounted } from 'vue'
+
+const isNavbarFloating = ref(false)
+
+function onScroll() {
+  isNavbarFloating.value = window.scrollY > 0
+}
+
+onMounted(() => {
+  window.addEventListener('scroll', onScroll)
+})
+
+onUnmounted(() => {
+  window.removeEventListener('scroll', onScroll)
+})
+</script>
+
 <template>
-  <nav class="d-flex justify-center w-100 position-fixed">
-    <a href="#home">Home</a>
-    <a href="#about">About</a>
-    <a href="#project">Project</a>
-  </nav>
+  <div class="d-flex justify-center">
+    <nav :class="{ 'navbar-floating': isNavbarFloating }">
+      <a href="#home">Home</a>
+      <a href="#about">About</a>
+      <a href="#project">Project</a>
+    </nav>
+  </div>
 </template>
 
 <style lang="scss" scoped>
 @import '@/assets/styles/variables';
 
 nav {
-  background: rgba(255, 255, 255, 0.1);
-  backdrop-filter: blur(10px);
+  border-radius: 50px;
+  display: flex;
+  align-items: center;
+  position: fixed;
   height: 60px;
+  margin-top: 20px;
+  transition: all 0.3s ease-out;
   z-index: 1000;
 
   a {
@@ -21,6 +45,7 @@ nav {
     position: relative;
     margin: 8px;
     padding: 8px;
+    font-weight: 600;
 
     &::after {
       content: '';
@@ -29,7 +54,7 @@ nav {
       bottom: 5px;
       width: 100%;
       height: 2px;
-      background: rgba(255, 255, 255, 0.5);
+      background: rgba(0, 0, 0, 0.5);
       transform: scaleX(0);
       transition: transform 0.3s ease;
       transform-origin: right;
@@ -40,5 +65,12 @@ nav {
       transform-origin: left;
     }
   }
+}
+
+.navbar-floating {
+  padding: 0 10px;
+  background: rgba(255, 255, 255, 0.5);
+  backdrop-filter: blur(10px);
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
 }
 </style>
